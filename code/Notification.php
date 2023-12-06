f8a54c8900f615fa494ff95ba90e20a1b7ef55f5
jcharra <johannes.charra@gmail.com>
2023-12-06 16:45:51 +0100

    add phpmailer & integration

--- code/Notification.php
+++ code/Notification.php
@@ -1,5 +1,15 @@
 <?php
 require_once('Util.php');
+require_once('email.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'inc/PHPMailer/Exception.php';
+require 'inc/PHPMailer/PHPMailer.php';
+require 'inc/PHPMailer/SMTP.php';
+
 
 function sendCreationNotificationMail($slotId)
 {
@@ -36,11 +46,11 @@ function sendCreationNotificationMail($slotId)
     $headers[] = 'Content-type: text/html; charset=utf-8';
 
     if (!empty($studentEmail)) {
-        mail($studentEmail, "Terminbestätigung " . $teacherName . " am " . toDate($date, "d.m.Y H:i") . " Uhr", $emailTemplateStudent, implode("\r\n", $headers));
+        sendMail($studentEmail, "Terminbestätigung " . $teacherName . " am " . toDate($date, "d.m.Y H:i") . " Uhr", $emailTemplateStudent);
     }
 
     if (!empty($teacherEmail)) {
-        mail($teacherEmail, "Neue Terminbuchung von " . $studentName . " am " . toDate($date, "d.m.Y H:i") . " Uhr", $emailTemplateTeacher, implode("\r\n", $headers));
+        sendMail($teacherEmail, "Neue Terminbuchung von " . $studentName . " am " . toDate($date, "d.m.Y H:i") . " Uhr", $emailTemplateTeacher);
     }
 }
 
@@ -76,14 +86,44 @@ function sendCancellationNotificationMail($slotId)
         "<div>" .
         "<div>Viele Grüße, <br> Die Elternsprechtag-Admins</div>";
 
-    $headers[] = 'MIME-Version: 1.0';
-    $headers[] = 'Content-type: text/html; charset=utf-8';
 
     if (!empty($studentEmail)) {
-        mail($studentEmail, "Terminabsage " . $teacherName . " am " . toDate($date, "d.m.Y H:i") . " Uhr", $emailTemplateStudent, implode("\r\n", $headers));
+        sendMail($studentEmail, "Terminabsage " . $teacherName . " am " . toDate($date, "d.m.Y H:i") . " Uhr", $emailTemplateStudent);
     }
 
     if (!empty($teacherEmail)) {
-        mail($teacherEmail, "Terminabsage von " . $studentName . " am " . toDate($date, "d.m.Y H:i") . " Uhr", $emailTemplateTeacher, implode("\r\n", $headers));
+        sendMail($teacherEmail, "Terminabsage von " . $studentName . " am " . toDate($date, "d.m.Y H:i") . " Uhr", $emailTemplateTeacher);
     }
+}
+
+function sendMail($to, $subject, $body)
+{
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host = SMTPConfig::$SMTP_HOST;
+        $mail->SMTPAuth = true;
+        $mail->Username = SMTPConfig::$SMTP_LOGIN;
+        $mail->Password = SMTPConfig::$SMTP_PWD;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
+        $mail->CharSet = "UTF-8";
+
+        //Recipients
+        $mail->setFrom(SMTPConfig::$SMTP_FROM);
+        $mail->addAddress($to);
+
+        //Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
 }
