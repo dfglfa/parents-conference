f19ae2ceef96971156fea1d5c2bcc514aa47d68a
jcharra <johannes.charra@gmail.com>
2025-12-01 12:48:09 +0100

    introduce boolean config variable ATTENDANCES_FROZEN to deny changes to attendance times for teachers

--- teacher.php
+++ teacher.php
@@ -13,99 +13,103 @@ include_once 'inc/header.php';
 
 <div class='container'>
 
-    <?php
-    printAlertForTemplate("teacherOverview");
-    ?>
+  <?php
+  printAlertForTemplate("teacherOverview");
+  ?>
 
-    <h1>Meine Übersicht</h1>
+  <h1>Meine Übersicht</h1>
 
-    <div class='panel-group' id='accordion'>
-
-
-        <div class='panel panel-default'>
-            <div class='panel-heading'>
-                <h4 class='panel-title'>
-                    <a data-toggle='collapse' data-parent='#accordion' href='#collapse2'>
-                        Meine Anwesenheit
-                    </a>
-                </h4>
-            </div>
-            <div id='collapse2' class='panel-collapse collapse'>
-                <div class='panel-body'>
-
-                    <h4>
-                        Aktuelle Anwesenheit
-                    </h4>
-                    <p id='attendance'>
-                        <?php
-                        $viewController = ViewController::getInstance();
-                        $attendance = $viewController->action_attendance();
-                        $event = EventDAO::getActiveEvent();
-                        $canChangeAttendance = $event != null && time() < $event->getStartPostDate();
-                        ?>
-                    </p>
-
-                    <?php if ($attendance != null && $canChangeAttendance): ?>
-                        <h4>
-                            Anwesenheit ändern
-                        </h4>
-                        <form id='changeAttendanceForm'>
-                            <input type='hidden' name='userId' value='<?php echo (escape($user->getId())) ?>'>
-                            <input type='hidden' name='eventId' value='<?php echo (escape($attendance['eventId'])) ?>'>
-                            <div class='form-group'>
-                                <select class='form-control' id='inputSlotDuration' name='inputFromTime'
-                                    style="width: 100px; display: inline-block">
-                                    <?php echo (getDateOptions($attendance, true)); ?>
-                                </select>
-                                <label style="padding: 0 20px;">bis</label>
-                                <select class='form-control' id='inputSlotDuration' name='inputToTime'
-                                    style="width: 100px; display: inline-block">
-                                    <?php echo (getDateOptions($attendance, false)); ?>
-                                </select>
-                            </div>
+  <div class='panel-group' id='accordion'>
 
 
-                            <button type='submit' class='btn btn-primary' id='btn-change-attendance'>
-                                Anwesenheit ändern
-                            </button>
+    <div class='panel panel-default'>
+      <div class='panel-heading'>
+        <h4 class='panel-title'>
+          <a data-toggle='collapse' data-parent='#accordion' href='#collapse2'>
+            Meine Anwesenheit
+          </a>
+        </h4>
+      </div>
+      <div id='collapse2' class='panel-collapse collapse'>
+        <div class='panel-body'>
 
-                        </form>
+          <h4>
+            Aktuelle Anwesenheit
+          </h4>
+          <p id='attendance'>
+            <?php
+            $viewController = ViewController::getInstance();
+            $attendance = $viewController->action_attendance();
+            $event = EventDAO::getActiveEvent();
+            $canChangeAttendance = !$ATTENDANCES_FROZEN && $event != null && time() < $event->getStartPostDate();
+            ?>
+          </p>
 
-                        <div class='message' id='message'></div>
-                    <?php else: ?>
-                        <div class="text-danger">Du kannst Deine Anwesenheit nicht mehr ändern, da Buchungen bereits möglich
-                            sind.</div>
-                    <?php endif; ?>
+          <?php if ($attendance != null): ?>
+            <h4>
+              Anwesenheit ändern
+            </h4>
+            <form id='changeAttendanceForm'>
+              <input type='hidden' name='userId' value='<?php echo (escape($user->getId())) ?>'>
+              <input type='hidden' name='eventId' value='<?php echo (escape($attendance['eventId'])) ?>'>
+              <div class='form-group'>
+                <select <?php echo !$canChangeAttendance ? "disabled" : ""; ?> class='form-control' id='inputSlotDuration' name='inputFromTime'
+                  style="width: 100px; display: inline-block">
+                  <?php echo (getDateOptions($attendance, true)); ?>
+                </select>
+                <label style="padding: 0 20px;">bis</label>
+                <select <?php echo !$canChangeAttendance ? "disabled" : ""; ?> class='form-control' id='inputSlotDuration' name='inputToTime'
+                  style="width: 100px; display: inline-block">
+                  <?php echo (getDateOptions($attendance, false)); ?>
+                </select>
+              </div>
+
+              <?php if ($canChangeAttendance): ?>
+                <button type='submit' class='btn btn-primary' id='btn-change-attendance'>
+                  Anwesenheit ändern
+                </button>
+              <?php else: ?>
+                <div class="text-danger">
+                  Änderungen der Anwesenheit sind nicht mehr möglich.
                 </div>
-            </div>
+              <?php endif; ?>
+
+            </form>
+
+            <div class='message' id='message'></div>
+          <?php else: ?>
+            <div class="text-danger">Es gibt aktuell keinen aktiven Sprechtag.</div>
+          <?php endif; ?>
         </div>
-
-        <div class='panel panel-default'>
-            <div class='panel-heading'>
-                <h4 class='panel-title'>
-                    <a data-toggle='collapse' data-parent='#accordion' href='#collapse1'>
-                        Mein Terminplan
-                    </a>
-                </h4>
-            </div>
-            <div id='collapse1' class='panel-collapse collapse in'>
-                <div class='panel-body'>
-                    <button class="btn btn-primary" onclick="window.print()">
-                        <span class='glyphicon glyphicon-print'></span>&nbsp;&nbsp;Zeitplan ausdrucken
-                    </button>
-
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" id="showempty">
-                            Freie Zeiträume ausblenden
-                        </label>
-                    </div>
-
-                    <div id='timeTable' class="section-to-print"></div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
+
+    <div class='panel panel-default'>
+      <div class='panel-heading'>
+        <h4 class='panel-title'>
+          <a data-toggle='collapse' data-parent='#accordion' href='#collapse1'>
+            Mein Terminplan
+          </a>
+        </h4>
+      </div>
+      <div id='collapse1' class='panel-collapse collapse in'>
+        <div class='panel-body'>
+          <button class="btn btn-primary" onclick="window.print()">
+            <span class='glyphicon glyphicon-print'></span>&nbsp;&nbsp;Zeitplan ausdrucken
+          </button>
+
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" id="showempty">
+              Freie Zeiträume ausblenden
+            </label>
+          </div>
+
+          <div id='timeTable' class="section-to-print"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php include_once 'inc/footer.php'; ?>
